992d0cb61e45b0f4d808f4c87bb2284c2667dc4f
Michael Chinn <mchinn614@gmail.com>
2018-12-27 11:23:09 -0800

    final version

--- index.js
+++ index.js
@@ -10,39 +10,42 @@ App will take user input of github handle then list all github repos on page
 
 function getRepos(userName){
     const url =`https://api.github.com/users/${userName}/repos`;
-    console.log(url);
     fetch(url)
     .then(response=>{
         if(response.ok){
             console.log(response)
             return response.json();
         };
-        console.log(response)
-        throw new Error(response.statusText);
+        throw new Error(response.status + '-' + response.statusText);
     })
     .then(responseJson=>{displayResults(responseJson)})
-    .catch(alert('Something Went Wrong'))
+    .catch(error=>{
+      $('.results-list').empty();
+      $('.results-list').append(`<h3>${error.name}: ${error.message}</h3>`);
+      $('section').removeClass('hidden');
+      })
 
 };
 
 function displayResults(resultList){
+    $('.results-list').empty()
     var i
-    for (i=0;i<2;i++){
+    console.log(resultList.length)
+    for (i=0;i<resultList.length;i++){
         const repoName = resultList[i].name;
         const repoLink = resultList[i].svn_url;
-        console.log(repoName)
-        console.log(repoList)
+        $('.results-list').append(`<ul><a href="${repoLink}">${repoName}</a></ul>`);
     }
+    $('section').removeClass('hidden');
 };
 
 function watchSubmit(){
-    $('form').on('submit',event=>{
+    $('.form').on('submit',event=>{
         event.preventDefault();
         const userName = $('.userName').val();
-        console.log(userName);
         getRepos(userName)
     })
 
 };
 
-$(watchSubmit());
+$(watchSubmit);
